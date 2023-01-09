5ed6924e9b4281d3401b9361e1c2805deee6fb75
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-01-09 18:05:04 +0300

    chore: add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>String Cutter</title>
+        <meta
+          name="description"
+          content="Cut long strings into smaller chunks"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
